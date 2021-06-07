2630ead55857bed2f9bce0153305413ac7c65664
Tc Bots <85434104+TcBots@users.noreply.github.com>
2021-06-08 01:11:42 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"✨ Hey ! This is the Music Assistant of @TcPlayerBot !!✨.\n\n ❗️ Rules:\n   - ⚠ No Chatting or Spam Allowed ⚠\n\n 👉 **ℹ️ Send Group Invite Link if Userbot Can't Join Your Group or Contact @supportgroup**")
+  await USER.send_message(message.chat.id,"✨ Hey ! This is the Music Assistant of @TcPlayerBot !!✨.\n\n ❗️ Rules:\n   - ⚠ No Chatting or Spam Allowed ⚠\n\n 👉 **ℹ️ Send Group Invite Link if Userbot Can't Join Your Group or Contact @TcBotsBugs**")
   return                        
