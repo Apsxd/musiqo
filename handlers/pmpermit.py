f71f7a3d33ebc0db05122e8596b0a2cde926a09c
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-04 08:40:01 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"[✨](https://telegra.ph/file/f6fad124b1ffd3cf8eefb.png) Heya! This is the Music Assistant of [Musiqo](https://t.me/BlissMusicRobot)")
+  await USER.send_message(message.chat.id,"[✨](https://telegra.ph/file/f6fad124b1ffd3cf8eefb.png) Heya! This is the Music Assistant of [Musiqo](https://t.me/MusiqoRobot)")
   return                        
