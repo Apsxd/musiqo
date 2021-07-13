364664e8948377ee99ab38093b70b5d62687cb26
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-13 13:09:39 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -75,173 +75,24 @@ async def start(client: Client, message: Message):
         )
     )
 
-# Help Menu
-
-@Client.on_message(command(["help", f"help@{BOT_USERNAME}"]))
-async def help(_, message: Message):
-    usr_cmd = message.text.split("_")[-1]
-    if usr_cmd == "/help":
-        chat_id = message.chat.id
-        if not await db.is_user_exist(chat_id):
-            await db.add_user(chat_id)
-            await Client.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"**📢 News ** \n#New_Music_Lover **Started To Using Meh!** \n\nFirst Name: `{message.from_user.first_name}` \nUser ID: `{message.from_user.id}` \nProfile Link: [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
-        parse_mode="markdown"
-    )
+@Client.on_message(
+    filters.command("help")
+    & filters.private
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>Hi {message.from_user.mention} 😉️!</b>
-
-**Here is the Help Menu For This Bot 😊!**""",
+        f"""<b>Hi {message.from_user.first_name}!
+</b>Join our group for reporting issues and bugs check commands [click here](https://telegra.ph/𝚖𝚞𝚜𝚒𝚚𝚘-Sᴏɴɢ-06-09) use @missblissrobot for downloading songs
+ </b>""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🤔 How To Use Me 🤔", callback_data="cbhowtouse"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Get Lyrics", callback_data="cbgetlyrics"
+                        "SOURCE CODE", url="https://github.com/LucidoXD/musiqo"
                     ),
                     InlineKeyboardButton(
-                        "YT Search", callback_data="cbytsearch"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Music Downloader", callback_data="cbmusicdown"
-                    ),
-                    InlineKeyboardButton(
-                        "YT Video Downloader", callback_data="cbytviddown"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Delete Commands", callback_data="cbdelcmds"
-                    ),
-                    InlineKeyboardButton(
-                        "Quotely", callback_data="cbquotely"
-                    )
-                ]
-            ]
-        )
-    )
-
-
-@Client.on_message(command("credits") & other_filters2)
-async def credits2(_, message: Message):
-    usr_cmd = message.text.split("_")[-1]
-    if usr_cmd == "/credits":
-        chat_id = message.chat.id
-        if not await db.is_user_exist(chat_id):
-            await db.add_user(chat_id)
-            await Client.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"**📢 News ** \n#New_Music_Lover **Started To Using Meh!** \n\nFirst Name: `{message.from_user.first_name}` \nUser ID: `{message.from_user.id}` \nProfile Link: [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
-        parse_mode="markdown"
-    )
-    await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
-__Note!__ ⚠️: This Project Is <b>Not Fully Owned By Me</b> !
-
-Credits To,
-
-<b><a href="https://github.com/CallsMusic">CallsMusic</a></b> - For Callsmusic (Main Code ❤️) !
-<b>Mr Dark Prince</b>
-<b>TheHamkercat</b>
-<b>AbirHasan2005</b>
-<b>DevsExpo</b>
-<b>TeamDaisyX</b>
-<b>N A C</b>
-
-Made with ❤️ by **@{UPDATES_CHANNEL}**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "🔰️ My Update Channel 🔰️", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
-                    )
-                ]
-            ]
-        ),
-        disable_web_page_preview=True
-    )   
-
-
-@Client.on_message(command(["vc", f"vc@{BOT_USERNAME}"]) & other_filters)
-async def vc(_, message: Message):
-    usr_cmd = message.text.split("_")[-1]
-    if usr_cmd == "/vc":
-        chat_id = message.chat.id
-        if not await db.is_user_exist(chat_id):
-            await db.add_user(chat_id)
-            await Client.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"**📢 News ** \n#New_Music_Lover **Started To Using Meh!** \n\nFirst Name: `{message.from_user.first_name}` \nUser ID: `{message.from_user.id}` \nProfile Link: [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
-        parse_mode="markdown"
-    )
-    VC_LINK = f"https://t.me/{message.chat.username}?voicechat"
-    await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-
-
-             😌️  **Voice Chat Link** 😌️
-____________________------------______________________
-
-👉️ [Here Is Your Voice Chat Link](https://t.me/{message.chat.username}?voicechat) 👈️
-____________________------------______________________
-
-Enjoy 😌️❤️!""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "↗️ Share Voice Chat Invitation ↗️", url=f"https://t.me/share/url?url=**Join%20Our%20Group%20Voice%20Chat%20😉%20%20{VC_LINK}%20❤️**"
-                    )
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🔰️ Update Channel 🔰️", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    ),
-                    InlineKeyboardButton(
-                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
-                    )
-                ]
-            ]
-        ),
-        disable_web_page_preview=True
-    )
-
-    
-@Client.on_message(command(["search", f"search@{BOT_USERNAME}"]))
-async def search(_, message: Message):
-    usr_cmd = message.text.split("_")[-1]
-    if usr_cmd == "/search":
-        chat_id = message.chat.id
-        if not await db.is_user_exist(chat_id):
-            await db.add_user(chat_id)
-            await Client.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"**📢 News ** \n#New_Music_Lover **Started To Using Meh!** \n\nFirst Name: `{message.from_user.first_name}` \nUser ID: `{message.from_user.id}` \nProfile Link: [{message.from_user.first_name}](tg://user?id={message.from_user.id})",
-        parse_mode="markdown"
-    )
-    await message.reply_text(
-        "💁🏻‍♂️ Do you want to search for a YouTube video?",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "✅ Yeah", switch_inline_query_current_chat=""
-                    ),
-                    InlineKeyboardButton(
-                        "Nope ❌", callback_data="close"
+                        "WEBSITE", url="https://cutt.ly/GjBGQ0D"
                     )
                 ]
             ]
