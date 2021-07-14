2f3e2ef977ac1ac3f1fdcf3a840f9b0575a5ed1a
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-14 21:50:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
