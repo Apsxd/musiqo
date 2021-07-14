a2960d6d83c20bb49e254b10666183d3750b7789
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-14 16:08:56 +0530

    Update queues.py

--- callsmusic/queues/queues.py
+++ callsmusic/queues/queues.py
@@ -1,42 +1,92 @@
-from asyncio import Queue, QueueEmpty as Empty
-from typing import Dict, Union
+from typing import Dict
 
-queues: Dict[int, Queue] = {}
+from pytgcalls import GroupCall
 
+from . import queues
+from . import client
 
-async def put(chat_id: int, **kwargs) -> int:
-    if chat_id not in queues:
-        queues[chat_id] = Queue()
-    await queues[chat_id].put({**kwargs})
-    return queues[chat_id].qsize()
+instances: Dict[int, GroupCall] = {}
+active_chats: Dict[int, Dict[str, bool]] = {}
 
 
-def get(chat_id: int) -> Union[Dict[str, str], None]:
-    if chat_id in queues:
-        try:
-            return queues[chat_id].get_nowait()
-        except Empty:
-            return None
+def init_instance(chat_id: int):
+    if chat_id not in instances:
+        instances[chat_id] = GroupCall(client)
+
+    instance = instances[chat_id]
+
+    @instance.on_playout_ended
+    async def ___(__, _):
+        queues.task_done(chat_id)
+
+        if queues.is_empty(chat_id):
+            await stop(chat_id)
+        else:
+            instance.input_filename = queues.get(chat_id)["file"]
 
 
-def is_empty(chat_id: int) -> bool:
-    if chat_id in queues:
-        return queues[chat_id].empty()
+def get_instance(chat_id: int) -> GroupCall:
+    init_instance(chat_id)
+    return instances[chat_id]
+
+
+async def start(chat_id: int):
+    await get_instance(chat_id).start(chat_id)
+    active_chats[chat_id] = {"playing": True, "muted": False}
+
+
+async def stop(chat_id: int):
+    await get_instance(chat_id).stop()
+
+    if chat_id in active_chats:
+        del active_chats[chat_id]
+
+
+async def set_stream(chat_id: int, file: str):
+    if chat_id not in active_chats:
+        await start(chat_id)
+    get_instance(chat_id).input_filename = file
+
+
+def pause(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif not active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).pause_playout()
+    active_chats[chat_id]["playing"] = False
     return True
 
 
-def task_done(chat_id: int):
-    if chat_id in queues:
-        try:
-            queues[chat_id].task_done()
-        except ValueError:
-            pass
+def resume(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).resume_playout()
+    active_chats[chat_id]["playing"] = True
+    return True
 
 
-def clear(chat_id: int):
-    if chat_id in queues:
-        if queues[chat_id].empty():
-            raise Empty
-        else:
-            queues[chat_id].queue = []
-    raise Empty
+def mute(chat_id: int) -> int:
+    if chat_id not in active_chats:
+        return 2
+    elif active_chats[chat_id]["muted"]:
+        return 1
+
+    get_instance(chat_id).set_is_mute(True)
+    active_chats[chat_id]["muted"] = True
+    return 0
+
+
+def unmute(chat_id: int) -> int:
+    if chat_id not in active_chats:
+        return 2
+    elif not active_chats[chat_id]["muted"]:
+        return 1
+
+    get_instance(chat_id).set_is_mute(False)
+    active_chats[chat_id]["muted"] = False
+    return 0
