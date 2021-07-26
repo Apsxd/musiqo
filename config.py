5fcb1b3788384cd707d13c92fd783ff48709190c
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-26 13:14:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
