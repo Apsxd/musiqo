d646f22e34b8a90c8647ff7c3dfcffd7d7cdb506
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-27 09:58:31 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -32,9 +32,6 @@ import os
 import aiohttp
 import aiofiles
 import ffmpeg
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
 from config import que
 from Python_ARQ import ARQ
 import json
@@ -99,81 +96,6 @@ def time_to_seconds(time):
 
 # Change image size
 
-def changeImageSize(maxWidth, maxHeight, image):
-
-    widthRatio = maxWidth / image.size[0]
-
-    heightRatio = maxHeight / image.size[1]
-
-    newWidth = int(widthRatio * image.size[0])
-
-    newHeight = int(heightRatio * image.size[1])
-
-    newImage = image.resize((newWidth, newHeight))
-
-    return newImage
-
-async def generate_cover(requested_by, title, views, duration, thumbnail):
-
-    async with aiohttp.ClientSession() as session:
-
-        async with session.get(thumbnail) as resp:
-
-            if resp.status == 200:
-
-                f = await aiofiles.open("background.png", mode="wb")
-
-                await f.write(await resp.read())
-
-                await f.close()
-
-    image1 = Image.open("./background.png")
-
-    image2 = Image.open("image/foreground.png")
-
-    image3 = changeImageSize(1280, 720, image1)
-
-    image4 = changeImageSize(1280, 720, image2)
-
-    image5 = image3.convert("RGBA")
-
-    image6 = image4.convert("RGBA")
-
-    Image.alpha_composite(image5, image6).save("temp.png")
-
-    img = Image.open("temp.png")
-
-    draw = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype("image/font.otf", 32)
-
-    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
-
-    draw.text(
-
-        (205, 590), f"Duration: {duration}", (255, 255, 255), font=font
-
-    )
-
-    draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
-
-    draw.text((205, 670),
-
-        f"Request By: {requested_by}",
-
-        (255, 255, 255),
-
-        font=font,
-
-    )
-
-    img.save("final.png")
-
-    os.remove("temp.png")
-
-    os.remove("background.png")
-
- 
 
 @Client.on_message(
 
