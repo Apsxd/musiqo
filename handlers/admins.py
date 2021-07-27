71aefa6b765d76728e54b6e0bfb2727f751c9a53
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-27 20:54:52 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -102,9 +102,9 @@ async def skip(_, message: Message):
 
 
 @Client.on_message(
-    filters.command("reload")
+    filters.command("admincache")
 )
 @errors
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    #await message.reply_text("Admin cache refreshed ✅")
+    #await message.reply_text("Adminlist refreshed ✅")
