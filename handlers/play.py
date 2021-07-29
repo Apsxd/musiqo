c5508f5e61fa815ab9f8cc2606195a70c95d64ea
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2021-07-29 14:09:02 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -129,7 +129,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
     image1 = Image.open("./background.png")
 
-    image2 = Image.open("image/foreground.png")
+    image2 = Image.open("image/Musiqo.png")
 
     image3 = changeImageSize(1280, 720, image1)
 
@@ -147,19 +147,19 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
     font = ImageFont.truetype("image/font.otf", 32)
 
-    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
+    draw.text((205, 550), f"Song Title: {title}", (51, 215, 255), font=font)
 
     draw.text(
 
-        (205, 590), f"Duration: {duration}", (255, 255, 255), font=font
+        (205, 590), f"Song Duration: {duration}", (255, 255, 255), font=font
 
     )
 
-    draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text((205, 630), f"Song Views: {views}", (255, 255, 255), font=font)
 
     draw.text((205, 670),
 
-        f"Request By: {requested_by}",
+        f"Playinh for: {requested_by}",
 
         (255, 255, 255),
 
@@ -551,7 +551,7 @@ async def m_cb(b, cb):
 
             await cb.answer('streaming resumed')     
 
-    elif type_ == 'puse':         
+    elif type_ == 'pause':         
 
         if (
 
@@ -703,7 +703,7 @@ async def play(_, message: Message):
 
     except:
 
-        user.first_name =  "myr"
+        user.first_name =  "musiqo_Assistant"
 
     usar = user
 
@@ -759,7 +759,7 @@ async def play(_, message: Message):
 
                                   f"<b>Flood wait timeout \nUser {user.first_name} flooding request userbot cant join group, check @LovishMusic_bot banned here or contact support @tubots"
 
-                                  "<b>dev says  add @musiqo_Assistant and try again</b>",
+                                  "<b>add @musiqo_Assistant and try again</b>",
 
                               )
 
